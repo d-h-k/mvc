912ae662d7407753e6104ab9f7ed5c2b3ff40fd8
d-h-k <ultraraptor@naver.com>
2021-11-02 17:21:37 +0900

    feat : spring-mvc 2단계 완성

---
+++ servlet/src/main/java/hello/servlet/web/servletmvc/v2/SpringMemberListControllerV2.java
@@ -0,0 +1,41 @@
+package hello.servlet.web.servletmvc.v2;
+
+import hello.servlet.domain.Member;
+import hello.servlet.domain.MemberRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+@Controller
+@RequestMapping("/springmvc/v2/members")
+public class SpringMemberListControllerV2 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/new-form")
+    public ModelAndView newForm() {
+        return new ModelAndView("new-form");
+    }
+
+    @RequestMapping("/save")
+    public ModelAndView save(HttpServletRequest request, HttpServletResponse response) {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(username, age);
+        System.out.println("member = " + member);
+        memberRepository.save(member);
+        return new ModelAndView("save-result").addObject("member",member);
+    }
+
+
+    @RequestMapping
+    public ModelAndView process(HttpServletRequest request, HttpServletResponse response) {
+        List<Member> members = memberRepository.findAll();
+        return new ModelAndView("members").addObject("members", members);
+    }
+}
