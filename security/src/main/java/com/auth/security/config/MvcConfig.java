818c1466c613a4d1712940256853f0ce77c0f8bd
d-h-k <ultraraptor@naver.com>
2021-12-07 09:53:15 +0900

    feat : jasypt 암호화 추가

--- security/src/main/java/com/auth/security/config/MvcConfig.java
+++
@@ -1,6 +0,0 @@
-package com.auth.security.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
---
+++ security/src/test/java/com/auth/security/config/JasyptConfigTest.java
@@ -0,0 +1,45 @@
+package com.auth.security.config;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class JasyptConfigTest {
+
+    @Test
+    void jasypt(){
+        String url = "jdbc:postgresql://localhost:5432/postgres";
+        String username = "postgres";
+        String password = "1234";
+
+        String encryptUrl = jasyptEncrypt(url);
+        String encryptUsername = jasyptEncrypt(username);
+        String encryptPassword = jasyptEncrypt(password);
+
+        System.out.println("encryptUrl : " + encryptUrl);
+        System.out.println("encryptUsername : " + encryptUsername);
+        System.out.println("encryptPassword" + encryptPassword);
+
+        assertThat(url).isEqualTo(jasyptDecryt(encryptUrl));
+    }
+
+    private String jasyptEncrypt(String input) {
+        String key = "5678";
+        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+        encryptor.setAlgorithm("PBEWithMD5AndDES");
+        encryptor.setPassword(key);
+        return encryptor.encrypt(input);
+    }
+
+    private String jasyptDecryt(String input){
+        String key = "5678";
+        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+        encryptor.setAlgorithm("PBEWithMD5AndDES");
+        encryptor.setPassword(key);
+        return encryptor.decrypt(input);
+    }
+
+}
