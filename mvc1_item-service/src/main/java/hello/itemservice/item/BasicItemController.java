634ea04a889c53961abf26f2e37b14adfa83bd7a
d-h-k <ultraraptor@naver.com>
2021-12-08 11:45:42 +0900

    feat : BasicItemController 추가

---
+++ mvc1_item-service/src/main/java/hello/itemservice/item/BasicItemController.java
@@ -0,0 +1,26 @@
+package hello.itemservice.item;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+
+    private final ItemRepository itemRepository; //final 키워드를 빼면 안된다!, 그러면 ItemRepository 의존관계 주입이 안됨
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+}
