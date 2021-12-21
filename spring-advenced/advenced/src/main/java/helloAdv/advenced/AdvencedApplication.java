d53d56202a5fc7b026a06d6c3708855b6d2c9b26
d-h-k <ultraraptor@naver.com>
2021-12-21 10:31:44 +0900

    feat : project config

---
+++ spring-advenced/advenced/src/main/java/helloAdv/advenced/AdvencedApplication.java
@@ -0,0 +1,13 @@
+package helloAdv.advenced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvencedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvencedApplication.class, args);
+	}
+
+}
---
+++ spring-advenced/advenced/src/test/java/helloAdv/advenced/AdvencedApplicationTests.java
@@ -0,0 +1,13 @@
+package helloAdv.advenced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvencedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
