dd4ce26ba2ee285b2db78404cb44bb35614d6d73
d-h-k <ultraraptor@naver.com>
2021-12-21 10:48:52 +0900

    feat : v0 로그 추적기 코드 구현

---
+++ spring-advenced/advenced/src/main/java/helloAdv/advenced/trace/TraceId.java
@@ -0,0 +1,46 @@
+package helloAdv.advenced.trace;
+
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Getter
+public class TraceId {
+    /* =======================
+    - 로그 추적기는 트랜잭션 ID && Depth/깊이 두가지를 동시에 표현해야한다
+    - 이 두가지를 묶어서 관리하기 위한 객체 TraceId 를 만들었다
+     */
+
+
+    private String id;
+    private int level;
+
+    //public Constructor
+    public TraceId() {
+        this.id = createdId();
+        this.level = 0;
+    }
+
+    //private Constructor
+    private TraceId(String id, int level) {
+        this.id = id;
+        this.level = level;
+    }
+
+    private String createdId() {
+        return UUID.randomUUID().toString().substring(0, 8);
+    }
+
+    private TraceId createNextId() {
+        return new TraceId(id, level + 1);
+    }
+
+    public TraceId creatPreviousId() {
+        return new TraceId(id, level - 1);
+    }
+
+    public boolean isFirstLevel() {
+        return level == 0;
+    }
+
+}
