c85086eaff30ffe0da8fd2fb690888b609e77046
d-h-k <ultraraptor@naver.com>
2021-12-28 19:38:20 +0900

    feat : LogTraceConfig 클래스 작성

---
+++ spring-advenced/advenced/src/main/java/helloadvanced/advenced/LogTraceConfig.java
@@ -0,0 +1,16 @@
+package helloadvanced.advenced;
+
+import helloadvanced.advenced.trace.logtrace.FieldLogTrace;
+import helloadvanced.advenced.trace.logtrace.LogTrace;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LogTraceConfig {
+
+    @Bean
+    public LogTrace logTrace {
+        return new FieldLogTrace();
+    }
+
+}
