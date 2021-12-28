55b1195530a15d44f9e330c0487832d02138eb34
d-h-k <ultraraptor@naver.com>
2021-12-28 21:08:13 +0900

    [2강] : 타이핑침

---
+++ spring-advenced/advenced/src/test/java/helloadvanced/advenced/trace/threadlocal/ThreadLocalServiceTest.java
@@ -0,0 +1,11 @@
+package helloadvanced.advenced.trace.threadlocal;
+
+import lombok.extern.slf4j.Slf4j;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+class ThreadLocalServiceTest {
+
+    
+}
