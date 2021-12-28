d9f9f1fd0dbf976698ff0be220fc177476ad5557
d-h-k <ultraraptor@naver.com>
2021-12-28 19:38:06 +0900

    feat : 2단윈 - V3 작성

---
+++ spring-advenced/advenced/src/main/java/helloadvanced/advenced/app/v3/OrderRepositoryV3.java
@@ -0,0 +1,39 @@
+package helloadvanced.advenced.app.v3;
+
+import helloadvanced.advenced.trace.TraceStatus;
+import helloadvanced.advenced.trace.hellotrace.HelloTraceV1;
+import helloadvanced.advenced.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV3 {
+
+    private final LogTrace trace;
+
+    public void save(String itemId) {
+
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderRepository.save()");
+            if (itemId.equals("ex")) {
+                throw new IllegalArgumentException("예외 발생!");
+            }
+            MySleep(1000);
+        } catch (Exception  e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+
+    private void MySleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
