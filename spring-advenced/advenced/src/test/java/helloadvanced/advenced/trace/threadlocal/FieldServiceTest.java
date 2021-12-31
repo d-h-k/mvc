2d9cd24df37abc02b992b58da4108fbdac72420a
d-h-k <ultraraptor@naver.com>
2021-12-31 23:30:25 +0900

    feat : [adv-쓰래드로컬] - v3 동시성 문제가 좀더 명확히 보이도록 코드 작성, "Thread Local 소개" 강좌 직전까지 끝냄

--- spring-advenced/advenced/src/test/java/helloadvanced/advenced/trace/threadlocal/FieldServiceTest.java
+++ spring-advenced/advenced/src/test/java/helloadvanced/advenced/trace/threadlocal/FieldServiceTest.java
@@ -1,32 +1,82 @@
 package helloadvanced.advenced.trace.threadlocal;
 
+import helloadvanced.advenced.trace.threadlocal.code.FieldService;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 @Slf4j
 class FieldServiceTest {
-    private FieldService fieldService = new FieldService();
+
+    private final FieldService fieldService = new FieldService();
 
     @Test
+    @DisplayName("동시성 이슈 없는 코드")
     void field() {
+
         log.info("main start");
+
         Runnable userA = () -> {
             fieldService.logic("userA");
         };
+
         Runnable userB = () -> {
             fieldService.logic("userB");
         };
+
         Thread threadA = new Thread(userA);
         threadA.setName("thread-A");
         Thread threadB = new Thread(userB);
         threadB.setName("thread-B");
+
         threadA.start(); //A실행
         sleep(2000); //동시성 문제 발생X
         // sleep(100); //동시성 문제 발생O
         threadB.start(); //B실행
-        sleep(3000); //메인 쓰레드 종료 대기
+
+        //메인 쓰레드 종료 대기
+        sleep(3000);
         log.info("main exit");
+        log.info("스래드가 끝나고 종료되게 만듬, 순서대로 실행됨");
     }
+
+    @Test
+    @DisplayName("동시성 이슈가 발생하는 코드")
+    void concurrencyProblemOccur() {
+
+        log.info("main start");
+
+        Runnable userA = () -> {
+            fieldService.logic("userA");
+        };
+
+        Runnable userB = () -> {
+            fieldService.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        threadA.start(); //A실행
+        //sleep(2000); //동시성 문제 발생X
+        sleep(100); //동시성 문제 발생O
+        threadB.start(); //B실행
+
+        //메인 쓰레드 종료 대기
+        sleep(3000);
+        log.info("main exit");
+        log.info("스래드가 끝나기 전에 쓰레드 하나 더 실행되서 동시성 문제가 발생함\n" +
+                         "  - 스레드 A 입장에서는 황당한데 \n" +
+                         "    - 여러 스래드가 같은 인스턴스의 필드에 접근하기 때문에\n" +
+                         "    - 서비스에서는 트래픽이 적을때는 발생하지 않다가\n" +
+                         "    - 장사가 잘되서 트래픽이 많아지면 점점 발생빈도가 증가하는 무시무시한 버그\n" +
+                         "    - 특히 스프링에서 치명적인데, 스프링은 싱글톤등록이 기본이기 때문에, 싱글톤객체의 필드를 변경할때 동시성 문제가 발생한다\n" +
+                         "    - 지역변수에서는 문제가 발생하지 않는데, 그 이유는 스레드마다 각각 다른 메모리 영역이 할당되기 떄문이고" +
+                         "    - 동시성 문제가 발생하는 곳은 주로 (공용필드/static + 읽기쓰기가 빈번한곳) = 싱글톤 빈의 인스턴스 필드에서 자주 발생");
+    }
+
 
     private void sleep(int millis) {
         try {
@@ -34,6 +84,19 @@ class FieldServiceTest {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    @Test
+    @DisplayName("Runnable 모르는 사람이 없게 해주세요")
+    public void studyJava() {
+        Runnable sample = new Runnable() {
+            @Override
+            public void run() {
+                //자바 기본이니까 알제??
+                fieldService.logic("sample");
+                log.info("모르면 자바 기본서좀 봐라 두번봐라 세번도 좋다");
+            }
+        };
     }
 }
 
--- spring-advenced/advenced/src/test/java/helloadvanced/advenced/trace/threadlocal/code/FieldService.java
+++ spring-advenced/advenced/src/test/java/helloadvanced/advenced/trace/threadlocal/code/FieldService.java
@@ -1,15 +1,16 @@
-package helloadvanced.advenced.trace.threadlocal;
+package helloadvanced.advenced.trace.threadlocal.code;
 
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class FieldService {
-    private String nameStore;
+
+    private String nameStore; // 필드변수에 저장
 
     public String logic(String name) {
         log.info("저장 name={} -> nameStore={}", name, nameStore);
-        nameStore = name;
+        nameStore = name;//로직이 실행되면 필드변수에 저장한다
         sleep(1000);
         log.info("조회 nameStore={}", nameStore);
         return nameStore;
