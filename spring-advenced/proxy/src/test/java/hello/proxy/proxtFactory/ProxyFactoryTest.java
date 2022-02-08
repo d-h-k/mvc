1ca4ed309462bcaa45102a631856499e3d09f174
d-h-k <ultraraptor@naver.com>
2022-02-08 16:25:03 +0900

    feat : [adv-W6-스프링프록시] 학습내용정리1

---
+++ spring-advenced/proxy/src/test/java/hello/proxy/proxtFactory/ProxyFactoryTest.java
@@ -0,0 +1,37 @@
+package hello.proxy.proxtFactory;
+
+import hello.proxy.common.advice.TimeAdvice;
+import hello.proxy.common.service.ServiceImpl;
+import hello.proxy.common.service.ServiceInterface;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.AopUtils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+public class ProxyFactoryTest {
+
+    @Test
+    @DisplayName("인터페이스가 있으면 JDK 동적프록시 사용")
+    public void InterfaceProxy() {
+
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.addAdvice(new TimeAdvice());
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+        log.info("JDK동프 >> targetClass={}", target.getClass());
+        log.info("JDK동프 >> proxyClass={}", proxy.getClass());
+
+        proxy.save();
+
+        assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+        assertThat(AopUtils.isJdkDynamicProxy(proxy)).isTrue();
+        assertThat(AopUtils.isCglibProxy(proxy)).isFalse();
+
+        log.info("물론 proxy.getClass() 로 인스턴스 정보도 찍을수 있다");
+        log.info("proxy.getClass() 인스턴스 정보>>> {}", proxy.getClass());
+    }
+}
