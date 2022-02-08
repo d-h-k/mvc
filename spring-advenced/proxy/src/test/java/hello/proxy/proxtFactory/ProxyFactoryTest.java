94e86b410efacc00ffc16af9243282df909057b6
d-h-k <ultraraptor@naver.com>
2022-02-08 16:29:27 +0900

    feat : [adv-W6-스프링프록시] 프록시의 타입을 확인하는 테스트코드 추가

--- spring-advenced/proxy/src/test/java/hello/proxy/proxtFactory/ProxyFactoryTest.java
+++ spring-advenced/proxy/src/test/java/hello/proxy/proxtFactory/ProxyFactoryTest.java
@@ -34,4 +34,31 @@ public class ProxyFactoryTest {
         log.info("물론 proxy.getClass() 로 인스턴스 정보도 찍을수 있다");
         log.info("proxy.getClass() 인스턴스 정보>>> {}", proxy.getClass());
     }
+
+    @Test
+    @DisplayName("ProxyTargetClass 옵션을 사용하면 인터페이스가 있어도 CGLIB 를 사용하고, 클래스기반의 프록시를 사용할수있어요")
+    public void proxyTargetClass() {
+
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        //여기가 중요하다
+        proxyFactory.setProxyTargetClass(true);
+        proxyFactory.addAdvice(new TimeAdvice());
+
+
+        ServiceInterface serviceInterfaceProxy = (ServiceInterface) proxyFactory.getProxy();
+        log.info("targetClass={}", target.getClass());
+        log.info("ServiceInterface serviceInterfaceProxy  >> proxyClass={}", serviceInterfaceProxy.getClass());
+
+        serviceInterfaceProxy.save();
+
+        assertThat(AopUtils.isAopProxy(serviceInterfaceProxy)).isTrue();
+        assertThat(AopUtils.isJdkDynamicProxy(serviceInterfaceProxy)).isFalse();
+        assertThat(AopUtils.isCglibProxy(serviceInterfaceProxy)).isTrue();
+
+        log.info("물론 serviceInterfaceProxy.getClass() 로 인스턴스 정보도 찍을수 있다");
+        log.info("serviceInterfaceProxy.getClass() 인스턴스 정보>>> {}", serviceInterfaceProxy.getClass());
+    }
+
+
 }
